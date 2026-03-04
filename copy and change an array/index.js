936c190f13675a4ccf953d8d4098ad5fa30ed081
arkarlin2003 <arkarlin486@gmail.com>
2026-03-04 16:11:51 +0630

    feat(app): copy and change an array

---
+++ copy and change an array/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+function copyArrayAndDivideBy2(array) {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] * 2);
+  }
+  return output;
+}
+
+const myArray = [1, 2, 3];
+const result = copyArrayAndDivideBy2(myArray);
