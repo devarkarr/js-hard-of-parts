5dda8dfb714badb1a24bd3b1bb3172653b115d55
arkarlin2003 <arkarlin486@gmail.com>
2026-03-04 16:27:21 +0630

    feat(app): pass a function as a argument

---
+++ pass a function as a argument/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+
+
+function copyArrayAndMultipulate(array, instruction) {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(instruction(array[i]));
+  }
+  return output;
+}
+
+function multiplyBy2(input) {
+  return input * 2;
+}
+
+const myArray = [1, 2, 3];
+
+const result = copyArrayAndDivideBy2(myArray, multiplyBy2);
